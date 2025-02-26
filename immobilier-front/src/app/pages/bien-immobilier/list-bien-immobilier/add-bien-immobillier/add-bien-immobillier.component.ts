6c582ddac08c3ab672d21e099ca960b3778b610e
mahayoussef12 <mayouss30@gmail.com>
2025-02-26 13:47:17 +0100

    commit

--- immobilier-front/src/app/pages/bien-immobilier/list-bien-immobilier/add-bien-immobillier/add-bien-immobillier.component.ts
+++ immobilier-front/src/app/pages/bien-immobilier/list-bien-immobilier/add-bien-immobillier/add-bien-immobillier.component.ts
@@ -1,11 +1,77 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {UserService} from "../../../../services/user.service";
+import {ImmobilierService} from "../../../../services/immobilier.service";
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatOption, MatSelect} from "@angular/material/select";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-add-bien-immobillier',
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardTitle,
+    NgIf,
+    NgClass,
+    ReactiveFormsModule,
+    MatLabel,
+    MatFormField,
+    MatInput,
+    MatError,
+    MatSelect,
+    MatOption,
+    NgForOf,
+    MatButton
+  ],
   templateUrl: './add-bien-immobillier.component.html',
+  standalone: true,
   styleUrl: './add-bien-immobillier.component.scss'
 })
-export class AddBienImmobillierComponent {
+export class AddBienImmobillierComponent implements OnInit{
+  form: FormGroup;
+  alertMessage: string | null = null;
+  alertType: 'success' | 'error' | null = null;
+
+  types = [
+    { value: 'Maison', viewValue: 'Maison' },
+    { value: 'Appartement', viewValue: 'Appartement' },
+    { value: 'Terrain', viewValue: 'Terrain' }
+  ];
+
+  constructor(private fb: FormBuilder,private service:ImmobilierService) {
+
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      titre: ['', [Validators.required, Validators.pattern('.*')]], // string obligatoire
+      description: ['', Validators.pattern('.*')], // string optionnel
+      adresse: ['', [Validators.required, Validators.pattern('.*')]], // string obligatoire
+      surface: [0, [Validators.required, Validators.min(1)]], // nombre obligatoire, min 1
+      nombrePieces: [0, [Validators.required, Validators.min(1)]], // nombre obligatoire, min 1
+      type: ['', [Validators.required, Validators.pattern('.*')]], // string obligatoire
+      prix: [0, [Validators.required, Validators.min(0)]], // nombre obligatoire, min 0
+      imageUrl: ['', Validators.pattern('.*')] // string optionnel
+    });
+  }
+
+    onSubmit(): void {
+    if (this.form.valid) {
+      console.log(this.form.value)
+     this.service.ajouterBien(this.form.value).subscribe(res=>{
+       this.alertMessage = 'Bien immobilier ajouté avec succès !';
+       this.alertType = 'success';
+
+     })
+    } else {
+      this.alertMessage = 'Veuillez remplir tous les champs obligatoires.';
+      this.alertType = 'error';
+    }
+  }
 
 }
--- immobilier-front/src/app/pages/bien-immobilier/list-bien-immobilier/update-bien-immobillier/update-bien-immobillier.component.ts
+++ immobilier-front/src/app/pages/bien-immobilier/list-bien-immobilier/update-bien-immobillier/update-bien-immobillier.component.ts
@@ -1,11 +1,75 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ImmobilierService} from "../../../../services/immobilier.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {FormsModule, NgForm, ReactiveFormsModule} from "@angular/forms";
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {MatFormField, MatInput, MatLabel} from "@angular/material/input";
+import {MatOption, MatSelect} from "@angular/material/select";
+import {MatError} from "@angular/material/form-field";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-update-bien-immobillier',
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardTitle,
+    MatError,
+    NgIf,
+    NgClass,
+    FormsModule,
+    MatInput,
+    MatLabel,
+    MatFormField,
+    MatSelect,
+    MatOption,
+    MatButton,
+    NgForOf,
+    ReactiveFormsModule
+  ],
   templateUrl: './update-bien-immobillier.component.html',
+  standalone: true,
   styleUrl: './update-bien-immobillier.component.scss'
 })
-export class UpdateBienImmobillierComponent {
+export class UpdateBienImmobillierComponent implements OnInit{
+  immobilier: any;
+  selectedType: string | undefined;
+  alertMessage: string | null = null;
+  alertType: 'success' | 'error' | null = null;
 
+  constructor(private router: Router,
+              private immobilierService: ImmobilierService,
+              private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.immobilierService.obtenirBienParId(this.route.snapshot.params['id']).subscribe(result => {
+      this.immobilier = result;
+      this.selectedType = this.immobilier.type;
+    });
+  }
+
+  updateImmobilier(form: NgForm): void {
+    if (form.invalid) {
+      form.form.markAllAsTouched();
+      this.alertMessage = 'Veuillez corriger les erreurs dans le formulaire.';
+      this.alertType = 'error';
+      return;
+    }
+
+    this.immobilierService.ajouterBien( this.immobilier).subscribe(
+      result => {
+        this.alertMessage = 'Mise à jour réussie.';
+        this.alertType = 'success';
+        setTimeout(() => {
+          this.router.navigate(['/Immobilier']);
+        }, 1000);
+      },
+      error => {
+        this.alertMessage = 'Erreur lors de la mise à jour du bien immobilier.';
+        this.alertType = 'error';
+      }
+    );
+  }
 }
